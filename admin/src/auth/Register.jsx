e4b6bf8ce578e0f8acfc737cef752f7de4f76fa0
arswe <abdurrahmansoftw@gmail.com>
2024-03-30 01:06:03 +0600

    Ecommerce

--- admin/src/auth/Register.jsx
+++ admin/src/auth/Register.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
 
 const Register = () => {
-  return <div>Register</div>
+  return (
+    <div className='min-w-screen min-h-screen bg-[#cdcae1] flex justify-center items-center '>
+      <div className='w-[350px] text-[#ffffff] p-2'>
+        <div className='bg-[#6f68d1] p-4 rounded-md'>
+          <h1 className='text-4xl font-bold text-center mb-3 '>Welcome to Ecommerce </h1>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Register
