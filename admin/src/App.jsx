af3c2cb993d8fa51c0fea0047bfa6ba919533541
arswe <abdurrahmansoftw@gmail.com>
2024-03-29 23:47:42 +0600

    add mui

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,5 +1,78 @@
+import { Box, CssBaseline, ToggleButton, ToggleButtonGroup } from '@mui/material'
+import { ThemeProvider, createTheme } from '@mui/material/styles'
+import { useState } from 'react'
+import getLPTheme from './themes/Themes'
+
+import AutoAwesomeRoundedIcon from '@mui/icons-material/AutoAwesomeRounded'
+import PropTypes from 'prop-types'
+
+import Navbar from './components/common/Navbar'
+
+function ToggleCustomTheme({ showCustomTheme, toggleCustomTheme }) {
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        width: '100dvw',
+        position: 'fixed',
+        bottom: 24,
+      }}
+    >
+      <ToggleButtonGroup
+        color='primary'
+        exclusive
+        value={showCustomTheme}
+        onChange={toggleCustomTheme}
+        aria-label='Platform'
+        sx={{
+          backgroundColor: 'background.default',
+          '& .Mui-selected': {
+            pointerEvents: 'none',
+          },
+        }}
+      >
+        <ToggleButton value>
+          <AutoAwesomeRoundedIcon sx={{ fontSize: '20px', mr: 1 }} />
+          Custom
+        </ToggleButton>
+        <ToggleButton value={false}>Default</ToggleButton>
+      </ToggleButtonGroup>
+    </Box>
+  )
+}
+
+ToggleCustomTheme.propTypes = {
+  showCustomTheme: PropTypes.shape({
+    valueOf: PropTypes.func.isRequired,
+  }).isRequired,
+  toggleCustomTheme: PropTypes.func.isRequired,
+}
+
 function App() {
-  return <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+  const [mode, setMode] = useState('light')
+  const [showCustomTheme, setShowCustomTheme] = useState(true)
+  const LPtheme = createTheme(getLPTheme(mode))
+  const defaultTheme = createTheme({ palette: { mode } })
+
+  const toggleColorMode = () => {
+    setMode((prev) => (prev === 'dark' ? 'light' : 'dark'))
+  }
+
+  const toggleCustomTheme = () => {
+    setShowCustomTheme((prev) => !prev)
+  }
+
+  return (
+    <ThemeProvider theme={showCustomTheme ? LPtheme : defaultTheme}>
+      <CssBaseline />
+      <Navbar mode={mode} toggleColorMode={toggleColorMode} />
+
+      <Box sx={{ bgcolor: 'background.default' }}>Hello</Box>
+      <ToggleCustomTheme showCustomTheme={showCustomTheme} toggleCustomTheme={toggleCustomTheme} />
+    </ThemeProvider>
+  )
 }
 
 export default App
