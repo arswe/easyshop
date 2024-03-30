fad5e843c79029a56b2e9a1705b5a39f97c351c3
arswe <abdurrahmansoftw@gmail.com>
2024-03-30 14:30:44 +0600

    Easy Shop Backend is Running

--- backend/server.js
+++ backend/server.js
@@ -9,9 +9,9 @@ dotenv.config()
 const port = process.env.PORT || 8080
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Easy Shop Backend is Running')
 })
 
 app.listen(port, () => {
-  console.log(color.rainbow(`backend port http://localhost:${process.env.PORT}`))
+  console.log(color.rainbow(`Backend port http://localhost:${process.env.PORT}`))
 })
