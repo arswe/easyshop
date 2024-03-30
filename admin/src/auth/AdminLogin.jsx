81e6b22e78b37384f2734d69db3a4fa7af5f6452
arswe <abdurrahmansoftw@gmail.com>
2024-03-31 02:40:10 +0600

    successMessage

--- admin/src/auth/AdminLogin.jsx
+++ admin/src/auth/AdminLogin.jsx
@@ -1,14 +1,19 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import { toast } from 'react-hot-toast'
 import { useDispatch, useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 import { PropagateLoader } from 'react-spinners'
-import { adminLogin } from '../store/reducers/authReducer'
+import { adminLogin, messageClear } from '../store/reducers/authReducer'
 
 const AdminLogin = () => {
   const [state, setState] = useState({ email: '', password: '' })
 
-  const dispatch = useDispatch()
+  const navigate = useNavigate()
 
-  const { loader } = useSelector((state) => state.auth)
+  const dispatch = useDispatch()
+  const { loader, errorMessage, successMessage } = useSelector(
+    (state) => state.auth
+  )
 
   const inputHandler = (e) => {
     setState({ ...state, [e.target.name]: e.target.value })
@@ -27,6 +32,18 @@ const AdminLogin = () => {
     itemsAlign: 'center',
     alignItems: 'center',
   }
+
+  useEffect(() => {
+    if (errorMessage) {
+      toast.error(errorMessage)
+      dispatch(messageClear())
+    }
+    if (successMessage) {
+      toast.success(successMessage)
+      dispatch(messageClear())
+      navigate('/')
+    }
+  }, [errorMessage, dispatch, successMessage, navigate])
 
   return (
     <div className='min-w-screen min-h-screen bg-[#cdcae1] flex justify-center items-center '>
@@ -56,7 +73,6 @@ const AdminLogin = () => {
                   type='email'
                   name='email'
                   id='email'
-                  
                   placeholder='Email'
                   required
                   className=' px-2  block w-full bg-transparent  rounded-md outline-none border-slate-400 border-0 py-1.5 text-white shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
--- admin/src/store/reducers/authReducer.js
+++ admin/src/store/reducers/authReducer.js
@@ -5,14 +5,13 @@ export const adminLogin = createAsyncThunk(
   'auth/adminLogin',
 
   async (info, { rejectWithValue, fulfillWithValue }) => {
-    console.log(info)
     try {
       const { data } = await apiClient.post('/adminLogin', info, {
         withCredentials: true,
       })
+
       return fulfillWithValue(data)
     } catch (error) {
-      // console.log(error.response.data)
       return rejectWithValue(error.response.data)
     }
   }
@@ -26,17 +25,28 @@ export const authReducer = createSlice({
     loader: false,
     userInfo: '',
   },
-  reducers: {},
+  reducers: {
+    messageClear: (state, _) => {
+      state.errorMessage = ''
+    },
+  },
 
   extraReducers: (builder) => {
-    builder.addCase(adminLogin.pending, (state, { payload }) => {
-      state.loader = true
-    })
-    builder.addCase(adminLogin.rejected, (state, { payload }) => {
-      state.loader = false
-      state.errorMessage = payload.error
-    })
+    builder
+      .addCase(adminLogin.pending, (state, { payload }) => {
+        state.loader = true
+      })
+      .addCase(adminLogin.rejected, (state, { payload }) => {
+        state.loader = false
+        state.errorMessage = payload.error
+      })
+      .addCase(adminLogin.fulfilled, (state, { payload }) => {
+        state.loader = false
+        state.successMessage = payload.success
+      })
   },
 })
+
+export const { messageClear } = authReducer.actions
 
 export default authReducer.reducer
