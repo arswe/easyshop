d8b184ccd643b6497516cc12c70d49758c915ab6
arswe <abdurrahmansoftw@gmail.com>
2024-03-31 01:46:31 +0600

    authControllers admin

--- backend/src/controllers/authControllers.js
+++ backend/src/controllers/authControllers.js
@@ -1,6 +1,7 @@
 const { responseReturn } = require('../exception/response')
 const adminModel = require('../models/adminModel')
 const bcrypt = require('bcrypt')
+const { createToken } = require('../utils/tokenCreate')
 
 class authControllers {
   adminLogin = async (request, response) => {
@@ -11,11 +12,15 @@ class authControllers {
       if (admin) {
         const match = await bcrypt.compare(password, admin.password)
         if (match) {
-          const token = admin.createToken({ id: admin._id, role: admin.role })
+          const token = await createToken({ id: admin._id, role: admin.role })
           response.cookie('accessToken', token, {
             expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
           })
-          responseReturn(response, 200, { message: 'Login success' })
+          responseReturn(response, 200, {
+            success: 'Login success',
+            token,
+            role: admin.role,
+          })
         } else {
           responseReturn(response, 400, { error: 'Password is incorrect' })
         }
