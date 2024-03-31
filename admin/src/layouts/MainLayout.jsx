dc9a6bf55fbeb3e6c0e312ce48f012e1e1d4d6d1
arswe <abdurrahmansoftw@gmail.com>
2024-03-31 20:40:13 +0600

    MainLayout

--- admin/src/layouts/MainLayout.jsx
+++ admin/src/layouts/MainLayout.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import Header from './Header'
+import SideBar from './SideBar'
 
 const MainLayout = () => {
   return (
-    <div>
-      MainLayout
-      <Outlet />
+    <div className='bg-[#b5f0f0] w-full min-h-screen'>
+      <Header />
+      <SideBar />
+
+      <div className='ml-0 lg:ml-[260px] pt-[95px] transition-all'>
+        Admin Main Layout
+        <Outlet />
+      </div>
     </div>
   )
 }
