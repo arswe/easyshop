3445e59db51ac393e503ad03c3c9fa638d57f876
arswe <abdurrahmansoftw@gmail.com>
2024-03-31 12:11:44 +0600

    authMiddleware

--- backend/src/middlewares/authMiddleware.js
+++ backend/src/middlewares/authMiddleware.js
@@ -2,5 +2,15 @@ const jwt = require('jsonwebtoken')
 
 module.exports.authMiddleware = async (request, response, next) => {
   const { accessToken } = request.cookies
-  
+
+  if (!accessToken) {
+    return response.status(401).json({ error: 'You need to login first' })
+  } else {
+    try {
+      const decodeToken = jwt.verify(accessToken, process.env.SECRET)
+      request.role = decodeToken.role
+      request.id = decodeToken.id
+      next()
+    } catch (error) {}
+  }
 }
