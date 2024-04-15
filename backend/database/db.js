6cd1a7a067b10ff77a898293ced7e000fc52e832
arswe <abdurrahmansoftw@gmail.com>
2024-04-15 23:32:25 +0600

    some update

--- backend/database/db.js
+++ backend/database/db.js
@@ -4,7 +4,7 @@ const color = require('colors')
 module.exports.dbConnect = async () => {
   try {
     await mongoose.connect(process.env.DB_URL)
-    console.log(color.rainbow('Database Connected..').bold)
+    console.log(color.rainbow('Easy Shop Database Connected..').bold)
   } catch (error) {
     console.log(color.red.bold(error.message))
   }
